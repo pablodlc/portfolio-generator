2f751a4b986b15430d2310d35f00c15a61153c4d
pablo de la cruz <pablodlc@gmail.com>
2021-05-03 21:46:56 -0700

    can generate HTML and linked two .js files

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
 
-// Notice the lack of parentheses around the `profileDataArr` parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+const generatePage = require('./src/page-template.js');
 
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+                                    //Content from 9.1, for reference 
+// console.log(profileDataArgs);
+
+// // Notice the lack of parentheses around the `profileDataArr` parameter?
+// const printProfileData = profileDataArr => {
+//     // This...
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+
+//     console.log('================');
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
